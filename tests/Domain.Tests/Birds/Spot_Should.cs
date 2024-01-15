0b9d86b7ac6e13bab39b39eaa50a1e8890e71bb2
Olivier-OG <78575869+Olivier-OG@users.noreply.github.com>
2024-01-15 10:55:23 +0100

    Try fix tests

--- tests/Domain.Tests/Birds/Spot_Should.cs
+++ tests/Domain.Tests/Birds/Spot_Should.cs
@@ -1,42 +1,50 @@
-﻿using System;
-using Domain.Birds;
+﻿using Domain.Birds;
 
 namespace Domain.Tests.Birds;
 
 // ReSharper disable once InconsistentNaming
 public class Spot_Should
 {
-    [Theory]
-    [InlineData(0.01, 0.01, "Olivier", "Up In A Tree")]    
-    [InlineData(0.03, 0.02, "Olivier", null)]
-    public void Be_Created_With_Valid_Params(double longitude, double latitude, string spotter, string? remark)
+    private static DateTime[] source =
     {
-        var act = () =>
-        {
-            Spot spot =  new(longitude, latitude, spotter, remark, DateTime.Today);
-        };
+        new(2024, 1, 16),
+        new(2023, 1, 13)
+    };
 
-        act.ShouldNotThrow();
-    }
-    
     [Theory]
-    [InlineData(0.01, 0.01, "Olivier", "Up In A Tree")]    
-    [InlineData(0.03, 0.02, "Olivier", null)]
-    public void Not_Be_Created_With_Invalid_SpottedOn(double longitude, double latitude, string spotter, string? remark)
+    [InlineData(0.01, 0.01, "Olivier", "Up In A Tree")]
+    [InlineData(0.02, 0.02, null, "Up In A Tree")]
+    [InlineData(0.03, 0.03, "Olivier", null)]
+    public void Be_Created_With_Valid_Params(double longitude, double latitude, string? spotter, string? remark)
     {
         var act = () =>
         {
-            Spot spot =  new(longitude, latitude, spotter, remark, DateTime.Today);
+            Spot spot = new(longitude, latitude, spotter, remark, DateTime.Now);
         };
 
         act.ShouldNotThrow();
     }
 
-    // [Theory]
-    // public void Throw_Exception_When_Set_Invalid_SpottedOn(DateTime spottedOn)
-    // {
-    //     
-    // }
+    [Theory]
+    [MemberData(nameof(source))]
+    public void Not_Be_Created_With_Invalid_SpottedOn(DateTime spottedOn)
+    {
+        var act = () =>
+        {
+            Spot spot = new(0.01, 0.01, "Olivier", "Up In A Tree", spottedOn);
+        };
 
+        act.ShouldThrow<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(source))]
+    public void Throw_Exception_When_Set_Invalid_SpottedOn(DateTime spottedOn)
+    {
+        Spot spot = new(0.01, 0.01, "Olivier", "Up In A Tree", DateTime.Now);
+
+        var act = () => { spot.SpottedOn = spottedOn; };
+
+        act.ShouldThrow<ArgumentOutOfRangeException>();
+    }
 }
-
